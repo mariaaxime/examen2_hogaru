8cf42e30933cd85e6a15b2fc2520994f3d79362c
Maria Acuna <mariaximenaam@gmail.com>
2018-01-24 16:10:39 +0000

    Add profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 20 }
 end
