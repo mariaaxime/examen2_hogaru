04588bd69698ff25f9e439d686ffc7c706b7d447
Maria Acuna <mariaximenaam@gmail.com>
2018-01-24 16:52:53 +0000

    Generate SeenAnnouncement model

---
+++ app/models/seen_announcement.rb
@@ -0,0 +1,2 @@
+class SeenAnnouncement < ApplicationRecord
+end
---
+++ test/models/seen_announcement_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SeenAnnouncementTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
